c44f87421570054e9f365e9b5f5f99435a075b44
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-02-25 16:22:09 +0530

    alternative way for export

--- module.js
+++ module.js
@@ -1,7 +1,9 @@
 const names = require('./names')
 const sayHi = require('./utils')
+const data = require('./alternative_export')
 // console.log(names)
 
 sayHi('ibrahim')
 sayHi(names.jhon)
 sayHi(names.peter)
+console.log(data)
