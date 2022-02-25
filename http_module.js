1830f7170a276b07a2fd27b4a761eb13bbadb9b4
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-02-26 01:52:37 +0530

    http module basics

---
+++ http_module.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Welcome')
+    } else if (req.url === '/about') {
+        res.end('About')
+    } else {
+        res.end(`
+        <h1>Oops!</h1>
+        <p>We can't seem to find the page you are looking for</p>
+        `)
+    }
+})
+
+server.listen(5000)
