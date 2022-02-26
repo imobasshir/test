149a4eefd2799e542ae0a84e7a5bb5a65c89de20
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-02-27 00:58:46 +0530

    added html to node app

--- nodeFinal/server.js
+++ nodeFinal/server.js
@@ -1,10 +1,14 @@
 const http = require('http')
+const { readFileSync } = require('fs')
+
+const homePage = readFileSync('./index.html')
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'text/html' })
+    const url = req.url
 
     if (req.url === '/') {
-        res.write('<h1 style="text-align: center">Home Page</h1>')
+        res.write(homePage)
         res.end()
     } else if (req.url === '/about') {
         res.write('<h1 style="text-align: center">About Page</h1>')
