e6b8d2f9df5ffb2636d6be7be008328c71018a47
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-02-27 00:49:03 +0530

    basic server creation in node app

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const http = require('http')
 
-const server = http.createServer()
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
 
-server.on('request', (req, res) => {
-    res.end('Welcome')
+    if (req.url === '/') {
+        res.write('<h1 style="text-align: center">Home Page</h1>')
+        res.end()
+    } else if (req.url === '/about') {
+        res.write('<h1 style="text-align: center">About Page</h1>')
+        res.end()
+    } else {
+        res.write('<h1 style="text-align: center">Oops!!!</h1>')
+        res.end()
+    }
 })
 
 server.listen(5000)
-
