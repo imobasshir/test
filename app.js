d80faa01dd92f83f47a4b51402ca03f17b72b9e1
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-09-26 00:04:01 +0530

    done

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require('express')
-const res = require('express/lib/response')
+const path = require('path')
 const app = express()
 
+app.use(express.static('./public'))
+
 app.get('/', (req, res) => {
-    res.status(200).send('Home Page')
-})
-app.get('/about', (req, res) => {
-    res.status(200).send('About Page')
+    res.sendFile(path.join(__dirname, './index.html'))
 })
 
 app.all('*', (req, res) => {
-    res.status(404).send('<h1 style="text-align: center">Resource not found</h1>')
+    res.status(404).send('<h3 style="text-align: center">404 Not Found</h3>')
 })
 
 app.listen(5000, () => {
     console.log(`Server is listening on port 5000.....`)
 })
 
-5; 03; 05
+// 5; 03; 05
